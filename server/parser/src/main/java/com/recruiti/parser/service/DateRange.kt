256eff4e4e59b268e63b2fedc3276c912fbe0705
Bohdan <doktor.bog@gmail.com>
2024-09-30 21:44:28 +0300

    date range and count params

---
+++ server/parser/src/main/java/com/recruiti/parser/service/DateRange.kt
@@ -0,0 +1,41 @@
+package com.recruiti.parser.service
+
+import com.recruiti.parser.data.Vacancy
+import java.time.LocalDate
+
+sealed class DateRange(val value: String) {
+    data object LastWeek : DateRange("lastWeek")
+    data object LastMonth : DateRange("lastMonth")
+    data object LastThreeMonths : DateRange("lastThreeMonths")
+    data object AllTime : DateRange("allTime")
+
+    companion object {
+        fun String?.asDateRange(): DateRange {
+            return when (this) {
+                LastWeek.value -> LastWeek
+                LastMonth.value -> LastMonth
+                LastThreeMonths.value -> LastThreeMonths
+                else -> AllTime
+            }
+        }
+    }
+}
+
+fun List<Vacancy>.filterByDateRange(dateRange: DateRange): List<Vacancy> {
+    if (dateRange is DateRange.AllTime) {
+        return this
+    }
+    val currentDate = LocalDate.now()
+    val dateThreshold = when (dateRange) {
+        DateRange.LastWeek -> currentDate.minusWeeks(1)
+        DateRange.LastMonth -> currentDate.minusMonths(1)
+        DateRange.LastThreeMonths -> currentDate.minusMonths(3)
+        else -> null
+    }
+    return this.asSequence()
+        .filter { vacancy ->
+            val dateParsed = vacancy.dateParsed
+            dateParsed != null && dateParsed.isAfter(dateThreshold)
+        }
+        .toList()
+}
--- server/parser/src/main/java/com/recruiti/parser/service/ParserService.kt
+++ server/parser/src/main/java/com/recruiti/parser/service/ParserService.kt
@@ -3,7 +3,9 @@ package com.recruiti.parser.service
 import com.recruiti.parser.data.asTable
 import com.recruiti.parser.data.createClient
 import com.recruiti.parser.domain.ParserRepository
+import com.recruiti.parser.service.DateRange.Companion.asDateRange
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -11,16 +13,29 @@ import io.ktor.server.routing.*
 fun Application.parser() {
     routing {
         get("/parser/query={query}") {
-            val queryAsText = call.parameters["query"]
-            if (!queryAsText.isNullOrBlank()) {
+            val queryAsText = call.parameters["query"].orEmpty()
+            val count = call.request.queryParameters["count"]?.toIntOrNull() ?: Int.MAX_VALUE
+            val dateRange = call.request.queryParameters["dateRange"].asDateRange()
+
+            if (queryAsText.isNotBlank()) {
                 val repo = ParserRepository(createClient())
-                val result = repo.parseVacanciesDou(queryAsText)
-                call.respondText(
-                    contentType = ContentType.parse("text/html"),
-                    text = result.sortedByDescending { it.dateParsed }.asTable()
-                )
+
+                try {
+                    val result = repo.parseVacanciesDou(queryAsText)
+                    val filteredResults = result
+                        .sortedByDescending { it.dateParsed }
+                        .filterByDateRange(dateRange)
+                        .take(count)
+
+                    call.respondText(
+                        contentType = ContentType.parse("text/html"),
+                        text = filteredResults.asTable()
+                    )
+                } catch (e: Exception) {
+                    call.respondText("Error occurred during parsing: ${e.message}")
+                }
             } else {
-                call.respondText("Empty query")
+                call.respondText("Empty query", status = HttpStatusCode.BadRequest)
             }
         }
     }
